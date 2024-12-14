5a2e7e8c4203c5574472a5e1bc65d25034979930
Ka <khw9899@Kas-MacBook-Pro.local>
2024-12-14 13:25:56 -0600

    day 14

---
+++ restroom-redoubt/solution-1.py
@@ -0,0 +1,110 @@
+from pathlib import Path
+import re
+
+current_working_directory = Path.cwd()
+input_file = open(str(current_working_directory) + "/input.txt", "r")
+data_lines = input_file.readlines()
+
+regex = r"-{0,1}\d+"
+
+
+def parse_inputs(lines):
+    robots_pos = []
+    robots_speed = []
+
+    for line in lines:
+        line = line.strip()
+        matches = re.finditer(regex, line, re.MULTILINE)
+
+        robot = []
+        curr_pair = []
+        for match in matches:
+            if len(curr_pair) == 1:
+                curr_pair = [int(match.group())] + curr_pair
+                robot.append(tuple(curr_pair))
+                curr_pair = []
+            else:
+                curr_pair = [int(match.group())] + curr_pair
+
+        robots_pos.append(robot[0])
+        robots_speed.append(robot[1])
+
+    return robots_pos, robots_speed
+
+
+def visualize_map(y_max, x_max, pos):
+    for y in range(y_max):
+        curr_line = ""
+        for x in range(x_max):
+            if (y, x) == pos:
+                curr_line += "1"
+            else:
+                curr_line += "."
+        print(curr_line)
+
+    print("\n")
+
+
+def solution():
+    y_max, x_max = (103, 101)
+    robots_pos, robots_speed = parse_inputs(data_lines)
+
+    t_max = 100
+    # visualize_map(y_max, x_max, robots_pos[0])
+
+    for _ in range(t_max):
+        next_robots_pos = []
+        for i in range(len(robots_pos)):
+            curr_pos = robots_pos[i]
+            curr_speed = robots_speed[i]
+            next_pos = (
+                (curr_pos[0] + curr_speed[0]) % y_max,
+                (curr_pos[1] + curr_speed[1]) % x_max,
+            )
+            next_robots_pos.append(next_pos)
+        robots_pos = next_robots_pos
+        # visualize_map(y_max, x_max, robots_pos[0])
+
+    quadrant_counts = [0, 0, 0, 0]
+    for pos in robots_pos:
+        # count each quadrant
+        y_cutoff = (y_max - 1) // 2
+        x_cutoff = (x_max - 1) // 2
+
+        y_pos, x_pos = pos
+
+        if x_pos < x_cutoff:
+            if y_pos < y_cutoff:
+                # quad 1
+                quadrant_counts[0] += 1
+            elif y_pos > y_cutoff:
+                # quad 3
+                quadrant_counts[2] += 1
+            else:
+                # not in any quadrant
+                continue
+        elif x_pos > x_cutoff:
+            if y_pos < y_cutoff:
+                # quad 2
+                quadrant_counts[1] += 1
+            elif y_pos > y_cutoff:
+                # quad 4
+                quadrant_counts[3] += 1
+            else:
+                # not in any quadrant
+                continue
+        else:
+            # not in any quadrant
+            continue
+
+    # print(quadrant_counts)
+
+    return (
+        quadrant_counts[0]
+        * quadrant_counts[1]
+        * quadrant_counts[2]
+        * quadrant_counts[3]
+    )
+
+
+print(solution())
